37536ef5133b5d4b9260e2ad438dc965174274b6
jb-aero <jb_aero@zeoldcraft.com>
2017-03-28 17:18:41 -0400

    testing

---
+++ ServiceTest/app/src/main/java/io/github/jb_aero/servicetest/MainActivity.java
@@ -0,0 +1,78 @@
+package io.github.jb_aero.servicetest;
+
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+	EditText value;
+	MyService service;
+	SensorManager sm;
+	Sensor accelerometer;
+	boolean bound;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Button button = (Button) findViewById(R.id.button);
+		button.setOnClickListener(this);
+
+		sm = (SensorManager) getSystemService(SENSOR_SERVICE);
+		accelerometer = sm.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+		value = (EditText) findViewById(R.id.value);
+		bound = false;
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		Intent intent = new Intent(this, MyService.class);
+		bindService(intent, serviceConnection, Context.BIND_AUTO_CREATE);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		if (bound) {
+			unbindService(serviceConnection);
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.button && bound) {
+			Toast.makeText(this, "Functioning!", Toast.LENGTH_SHORT).show();
+			// value.setText(String.valueOf(service.pollGravity()));
+		}
+	}
+
+	ServiceConnection serviceConnection = new ServiceConnection() {
+		@Override
+		public void onServiceConnected(ComponentName name, IBinder binder) {
+			MyService service = ((MyService.MyBinder) binder).getService();
+
+			sm.registerListener(service, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+			bound = true;
+		}
+
+		@Override
+		public void onServiceDisconnected(ComponentName name) {
+			sm.unregisterListener(service);
+			bound = false;
+		}
+	};
+}
---
+++ ServiceTest/app/src/main/java/io/github/jb_aero/servicetest/MyService.java
@@ -0,0 +1,52 @@
+package io.github.jb_aero.servicetest;
+
+import android.app.Service;
+import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Binder;
+import android.os.Handler;
+import android.os.IBinder;
+
+public class MyService extends Service implements SensorEventListener {
+
+	MyBinder binder;
+	double cgx, cgy, cgz, // current values (t)
+			lgx, lgy, lgz; // last values (t - 1)
+
+	public MyService() {
+		binder = new MyBinder();
+		lgx = lgy = lgz = 0;
+	}
+
+	public double pollGravity() {
+
+		return Math.sqrt((cgx * cgx) + (cgy * cgy) + (cgz * cgz));
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		return binder;
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		cgx = (0.9 * lgx) + (0.1 * event.values[0]);
+		cgy = (0.9 * lgy) + (0.1 * event.values[1]);
+		cgz = (0.9 * lgz) + (0.1 * event.values[2]);
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+	}
+
+	public class MyBinder extends Binder
+	{
+		public MyService getService() {
+			return MyService.this;
+		}
+	}
+}
